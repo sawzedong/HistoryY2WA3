899979363fe87a58436d26169584cc82c6a44a30
sawzedong <saw.zedong@dhs.sg>
2020-06-20 17:41:51 +0800

    Updates

--- precache-manifest.a3198ff3814d6b6636bab7dcaaaf07b1.js
+++ precache-manifest.a3198ff3814d6b6636bab7dcaaaf07b1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fdeaf312863ae2badfd701ba44be50c2",
+    "revision": "86e94a7937214b197913bac2ed33c75c",
     "url": "/HistoryY2WA3/index.html"
   },
   {
-    "revision": "bc143bc3342e113f8e8f",
+    "revision": "13d4c0a95b3ffbd8df56",
     "url": "/HistoryY2WA3/static/css/main.957a3edc.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/HistoryY2WA3/static/js/2.8cd5e81e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bc143bc3342e113f8e8f",
-    "url": "/HistoryY2WA3/static/js/main.19202b80.chunk.js"
+    "revision": "13d4c0a95b3ffbd8df56",
+    "url": "/HistoryY2WA3/static/js/main.db1b3fa6.chunk.js"
   },
   {
     "revision": "f8ea6975d00a024c0257",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HistoryY2WA3/precache-manifest.2bff8b8b0e927699a2319281a2cad1f1.js"
+  "/HistoryY2WA3/precache-manifest.a3198ff3814d6b6636bab7dcaaaf07b1.js"
 );
 
 self.addEventListener('message', (event) => {
