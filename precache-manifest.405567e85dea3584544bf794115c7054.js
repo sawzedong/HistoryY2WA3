78631627a81a62cedc1f512dd2731a194aca24ab
sawzedong <saw.zedong@dhs.sg>
2020-06-20 16:07:38 +0800

    Updates

---
+++ precache-manifest.405567e85dea3584544bf794115c7054.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d3943ed19b6c3ca1cfce43717bbda1",
+    "url": "/HistoryY2WA3/index.html"
+  },
+  {
+    "revision": "1d1e4219a870b5a77a4e",
+    "url": "/HistoryY2WA3/static/css/main.957a3edc.chunk.css"
+  },
+  {
+    "revision": "545bca019af8b9637634",
+    "url": "/HistoryY2WA3/static/js/2.8cd5e81e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HistoryY2WA3/static/js/2.8cd5e81e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1e4219a870b5a77a4e",
+    "url": "/HistoryY2WA3/static/js/main.b621584c.chunk.js"
+  },
+  {
+    "revision": "f8ea6975d00a024c0257",
+    "url": "/HistoryY2WA3/static/js/runtime-main.0f7de852.js"
+  },
+  {
+    "revision": "0d85b0d5e1c4e74588168cfcea5ef4d7",
+    "url": "/HistoryY2WA3/static/media/test-img.0d85b0d5.png"
+  }
+]);
