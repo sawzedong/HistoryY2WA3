e08b8c5b28025465ccdf7f78525e6314bd50dfbb
sawzedong <saw.zedong@dhs.sg>
2020-08-08 22:40:37 +0800

    Added filler text, reformatted code

--- src/pages/Time-Periods/time-period-3.js
+++ src/pages/Time-Periods/time-period-3.js
@@ -2,13 +2,12 @@
 import React from "react";
 import PageTemplate from "../../components/PageTemplate";
 
-
 export default function Interview3() {
   return (
     <PageTemplate padding>
       <div className="min-vh-100 vw-100">
         <div className="px-5 py-4">
-        <h1>Time Period 3</h1>
+          <h1>Time Period 3</h1>
         </div>
         <div className="row py-4">
           <div className="col-md-3 px-5">
@@ -23,39 +22,134 @@ export default function Interview3() {
             </p>
           </div>
           <div className="col-md-9 px-5">
-          <h2>Information</h2>
+            <h2>Information</h2>
             <table>
               <tr>
-                <td className="p-2 w-30"><h3>Housing</h3></td>
+                <td className="p-2 w-30">
+                  <h3>Housing</h3>
+                </td>
                 <td className="p-2 w-70">
                   <ul>
-                    <li>Yay</li>
-                    <li>Yay</li>
-                    <li>Yay</li>
-                    <li>Yay</li>
-                    <li>Yay</li>
-                    <li>Yay</li>
+                    <li>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </li>
+                    <li>
+                      Nullam cursus, diam nec consequat fermentum, mauris felis
+                      tincidunt lorem, ac rutrum enim dui a tellus.
+                    </li>
+                    <li>
+                      Etiam tristique suscipit velit. Donec sit amet blandit
+                      lacus.
+                    </li>
+                    <li>
+                      Interdum et malesuada fames ac ante ipsum primis in
+                      faucibus. Morbi hendrerit tortor a nulla eleifend
+                      dignissim.
+                    </li>
                   </ul>
                 </td>
               </tr>
               <tr>
-                <td className="p-2 w-30"><h3>Environment</h3></td>
-                <td className="p-2 w-70">SOMETHING</td>
+                <td className="p-2 w-30">
+                  <h3>Environment</h3>
+                </td>
+                <td className="p-2 w-70">
+                  <ul>
+                    <li>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </li>
+                    <li>
+                      Nullam cursus, diam nec consequat fermentum, mauris felis
+                      tincidunt lorem, ac rutrum enim dui a tellus.
+                    </li>
+                    <li>
+                      Etiam tristique suscipit velit. Donec sit amet blandit
+                      lacus.
+                    </li>
+                    <li>
+                      Interdum et malesuada fames ac ante ipsum primis in
+                      faucibus. Morbi hendrerit tortor a nulla eleifend
+                      dignissim.
+                    </li>
+                  </ul>
+                </td>
               </tr>
               <tr>
-                <td className="p-2 w-30"><h3>Population</h3></td>
-                <td className="p-2 w-70">SOMETHING</td>
+                <td className="p-2 w-30">
+                  <h3>Population</h3>
+                </td>
+                <td className="p-2 w-70">
+                  <ul>
+                    <li>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </li>
+                    <li>
+                      Nullam cursus, diam nec consequat fermentum, mauris felis
+                      tincidunt lorem, ac rutrum enim dui a tellus.
+                    </li>
+                    <li>
+                      Etiam tristique suscipit velit. Donec sit amet blandit
+                      lacus.
+                    </li>
+                    <li>
+                      Interdum et malesuada fames ac ante ipsum primis in
+                      faucibus. Morbi hendrerit tortor a nulla eleifend
+                      dignissim.
+                    </li>
+                  </ul>
+                </td>
               </tr>
               <tr>
-                <td className="p-2 w-30"><h3>Monetary</h3></td>
-                <td className="p-2 w-70">SOMETHING</td>
+                <td className="p-2 w-30">
+                  <h3>Monetary</h3>
+                </td>
+                <td className="p-2 w-70">
+                  <ul>
+                    <li>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </li>
+                    <li>
+                      Nullam cursus, diam nec consequat fermentum, mauris felis
+                      tincidunt lorem, ac rutrum enim dui a tellus.
+                    </li>
+                    <li>
+                      Etiam tristique suscipit velit. Donec sit amet blandit
+                      lacus.
+                    </li>
+                    <li>
+                      Interdum et malesuada fames ac ante ipsum primis in
+                      faucibus. Morbi hendrerit tortor a nulla eleifend
+                      dignissim.
+                    </li>
+                  </ul>
+                </td>
               </tr>
               <tr>
-                <td className="p-2 w-30"><h3>Social</h3></td>
-                <td className="p-2 w-70">SOMETHING</td>
+                <td className="p-2 w-30">
+                  <h3>Social</h3>
+                </td>
+                <td className="p-2 w-70">
+                  <ul>
+                    <li>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </li>
+                    <li>
+                      Nullam cursus, diam nec consequat fermentum, mauris felis
+                      tincidunt lorem, ac rutrum enim dui a tellus.
+                    </li>
+                    <li>
+                      Etiam tristique suscipit velit. Donec sit amet blandit
+                      lacus.
+                    </li>
+                    <li>
+                      Interdum et malesuada fames ac ante ipsum primis in
+                      faucibus. Morbi hendrerit tortor a nulla eleifend
+                      dignissim.
+                    </li>
+                  </ul>
+                </td>
               </tr>
             </table>
-            
           </div>
         </div>
       </div>
