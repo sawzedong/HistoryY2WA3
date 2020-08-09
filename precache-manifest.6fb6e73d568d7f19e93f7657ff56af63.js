9addb1a9d30d7cb1d2a9a138d91fd8b71b531570
sawzedong <saw.zedong@dhs.sg>
2020-08-09 14:16:43 +0800

    Updates

---
+++ precache-manifest.6fb6e73d568d7f19e93f7657ff56af63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43cff44f9c130cc1a4a7901c32116bb",
+    "url": "/HistoryY2WA3/index.html"
+  },
+  {
+    "revision": "0429ea4dabb937295568",
+    "url": "/HistoryY2WA3/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "113586d1ade116ebb1a9",
+    "url": "/HistoryY2WA3/static/css/main.4c214f5b.chunk.css"
+  },
+  {
+    "revision": "0429ea4dabb937295568",
+    "url": "/HistoryY2WA3/static/js/2.68d3ba4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HistoryY2WA3/static/js/2.68d3ba4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113586d1ade116ebb1a9",
+    "url": "/HistoryY2WA3/static/js/main.8fefbcbc.chunk.js"
+  },
+  {
+    "revision": "d9fc3765e8d32cdf245b",
+    "url": "/HistoryY2WA3/static/js/runtime-main.11f88daa.js"
+  },
+  {
+    "revision": "69bd419dea61853ced769442b078b7e7",
+    "url": "/HistoryY2WA3/static/media/homepage-img.69bd419d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HistoryY2WA3/precache-manifest.e43509cd28b66adba7bd5a1bbb1b94e2.js"
+  "/HistoryY2WA3/precache-manifest.6fb6e73d568d7f19e93f7657ff56af63.js"
 );
 
 self.addEventListener('message', (event) => {
